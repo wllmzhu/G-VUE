03d5eb5e099e06c3a2b7fcbe2a5bfc12b8a9f942
William Zhu <45189159+WilliamYZhu@users.noreply.github.com>
2022-05-05 18:43:17 +0800

    Revamping existing HOI code and data directory structure

--- scripts/setup_hoi.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ../data/hoi/hico_processed
-
-python -m preprocessing.hicodet.mat_to_json
-python -m preprocessing.hicodet.hoi_cls_count
-python -m preprocessing.hicodet.split_ids
--- scripts/setup_vqa.sh
+++
@@ -1,7 +0,0 @@
-# Setup
-mkdir ../datasets/data
-
-# Get GloVe Embeddings
-wget -P ../datasets/data http://nlp.stanford.edu/data/glove.6B.zip
-unzip ../datasets/data/glove.6B.zip -d ../datasets/data/GloVe
-rm ../datasets/data/glove.6B.zip
