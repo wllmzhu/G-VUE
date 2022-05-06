f3befc6ccfa55aefcb0599097ba8ff3b637fa483
William Zhu <45189159+WilliamYZhu@users.noreply.github.com>
2022-05-06 10:41:01 +0800

    Update HOI setup script

--- scripts/setup_hoi.sh
+++ scripts/setup_hoi.sh
@@ -1,8 +1,11 @@
+# Install gdown
 pip install gdown --user
 export PATH="$HOME/.local/bin:$PATH"
 
-mkdir -p ../data/hicodet
-cd ../data/hicodet
+# Go to HOI data directory
+HOI_PATH=../data/hicodet
+mkdir -p $HOI_PATH
+cd $HOI_PATH
 
 # Annotations: download hico.zip
 gdown 'https://drive.google.com/uc?id=1BanIpXb8UH-VsA9yg4H9qlDSR0fJkBtW'
@@ -13,10 +16,11 @@ rm hico.zip
 mv hico/* .
 rmdir hico
 
-# # # Images: download hico_20160224_det.tar.gz
-# # gdown 'https://drive.google.com/uc?id=1QZcJmGVlF9f4h-XLWe9Gkmnmj2z1gSnk'
-# # tar -xf hico_20160224_det.tar.gz
-# # rm hico_20160224_det.tar.gz
+# Images: download hico_20160224_det.tar.gz
+gdown 'https://drive.google.com/uc?id=1QZcJmGVlF9f4h-XLWe9Gkmnmj2z1gSnk'
+tar -xf hico_20160224_det.tar.gz
+rm hico_20160224_det.tar.gz
 
-# Extract image directory
-mv hico_20160224_det.tar.gz/images .
+Extract image directory
+mv hico_20160224_det/images .
+rm -rf hico_20160224_det
