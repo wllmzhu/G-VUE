948aa80b61b30f69ee9c06ea6ff900cf210b3ef2
William Zhu <williamzhu@g.ucla.edu>
2022-05-22 22:47:50 +0800

    Change ADE20k to using MIT Scene Parsing Benchmark

---
+++ datasets/README.md
@@ -0,0 +1,10 @@
+
+ADE20k (MIT Scene Parsing Benchmark) 
+
+Download index file at:
+https://github.com/CSAILVision/semantic-segmentation-pytorch/tree/e21b8e2bfb1cb145941c36468fc807f20146b71a
+
+Or use:
+wget https://raw.githubusercontent.com/CSAILVision/semantic-segmentation-pytorch/e21b8e2bfb1cb145941c36468fc807f20146b71a/data/ADE20K_object150_train.txt
+
+wget https://raw.githubusercontent.com/CSAILVision/semantic-segmentation-pytorch/e21b8e2bfb1cb145941c36468fc807f20146b71a/data/ADE20K_object150_val.txt
--- datasets/ade20k.py
+++ datasets/ade20k.py
@@ -4,12 +4,13 @@ import numpy as np
 import hydra
 from PIL import Image
 import torch
+import cv2
 from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import functional as F
 import utils.io as io
 from transforms.ade20k_transforms import make_ade20k_transforms
 from utils.misc import collate_fn
-from .base import DATASET
+from base import DATASET
 
 
 @DATASET.register()
@@ -21,57 +22,28 @@ class ADE20kDataset(Dataset):
         assert self.subset in self.info.subsets, f'subset {self.subset} not in {self.info.subsets}'
         self.transform = make_ade20k_transforms(subset)
         self._load_dataset()
-        self._load_class_dict()
     
     def _load_dataset(self):
-        with open(self.info.index_file, 'rb') as f:
-            self.index_ade20k = pkl.load(f)
-
-        self.train_idx = []
-        self.val_idx = []
-        for idx, filename in enumerate(self.index_ade20k['filename']):
-            if 'train' in filename:
-                self.train_idx.append(idx)
-            elif 'val' in filename:
-                self.val_idx.append(idx)
-        # Data also contains "frame" data like ADE_frame_00000001.jpg, ignored
-
-        print(f'load {self.__len__()} samples in ADE20k {self.subset}')
-    
-    def _load_class_dict(self):
-        with open(self.info.class_dict, 'r') as f:
-            idx2cat = f.readlines()
-        self.class_dict = {}
-        for line in idx2cat:
-            idx, cat = line.split()
-            idx, cat = int(idx), int(cat)
-            self.class_dict.update({cat: idx})
-        self.class_dict.update({0: 0})
-        print('constructed mapping between category and class index')
+        self.samples = [x.rstrip() for x in open(os.path.join(self.info.index_dir, f'ADE20K_object150_{self.subset}.txt'), 'r')]
+        print(f'load {len(self.samples)} samples in ADE20K (MIT Scene Parsing Benchmark) {self.subset}')
 
     def __len__(self):
-        return len(self.train_idx) if self.subset == 'train' else len(self.val_idx)
+        return len(self.samples)
 
     def __getitem__(self, i):
-        if self.subset == 'train':
-            i = self.train_idx[i]
-        else:
-            i = self.val_idx[i]
+        filename = self.samples[i]
+        img_filename = os.path.join(self.info.img_dir, filename)
+        seg_filename = os.path.join(self.info.anno_dir, filename)
+        seg_filename = seg_filename.replace('.jpg', '.png')
 
-        filename = os.path.join(self.info.top_dir, self.index_ade20k['folder'][i], self.index_ade20k['filename'][i])
-        fileseg = filename.replace('.jpg', '_seg.png')
+        img = Image.open(img_filename).convert('RGB')
+        seg = Image.open(seg_filename)
 
-        img = Image.open(filename)
+        seg = F.to_tensor(seg) - 1
 
-        with Image.open(fileseg) as io:
-            seg = np.array(io)
-        r = seg[:,:,0]
-        g = seg[:,:,1]
-        category = (r/10).astype(np.int32)*256+(g.astype(np.int32))
-        category = F.to_tensor(category)
-        category.apply_(self.class_dict.get)
+        target = {
+                'depth': seg}
 
-        target = {'depth': category}
         img, target = self.transform(img, target)
 
         # image, segmentation
@@ -86,7 +58,8 @@ def main(cfg):
     dataset = ADE20kDataset(cfg.dataset.info, 'val')
     dataloader = dataset.get_dataloader(batch_size=8, shuffle=False)
     for data in dataloader:
-        imgs, targets = data
+        print(len(data))
+        imgs, txt, targets = data
         print({
             'image': imgs[0],
             'target': targets[0],
